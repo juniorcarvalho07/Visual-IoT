9dde8278af2fc10e520a1615631aed3bc7758c12
juniorcarvalho07 <junior_sousa95@hotmail.com>
2017-09-18 14:32:31 -0300

    Segundo commit

--- PDSI2_Visual_IoT.ino
+++ PDSI2_Visual_IoT.ino
@@ -1,6 +1,15 @@
+#include <Ultrasonic.h>
+
+//Pinos trigger e echo usado para leitura do sensor.
+#define pino_trigger 4
+#define pino_echo 5
+
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
 
 void setup() {
+
+  Serial.begin(9600);
   // put your setup code here, to run once:
 
 }
