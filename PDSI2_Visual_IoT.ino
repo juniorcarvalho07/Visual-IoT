70f9baa9e353a37cb6887e0af6efdf36592cebd1
juniorcarvalho07 <junior_sousa95@hotmail.com>
2017-09-18 19:49:11 -0300

    Teste 1

--- PDSI2_Visual_IoT.ino
+++ PDSI2_Visual_IoT.ino
@@ -7,13 +7,20 @@
 
 Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
+void teste_Ultrasonic(){
+  long microsec = 0;
+  do{
+  microsec = ultrasonic.timing();
+  }while(microsec == 1023);
+}
 void setup() {
   //Abri a comunicação serial com a porta 9600;
   Serial.begin(9600);
 }
 
 void loop() {
-  
+  // 1 teste sensor Ultrasonic
+  teste_Ultrasonic();
   //ler o sensor retornando o o valor do sinal;
   long microsec = ultrasonic.timing();
   // retornar em centimetros a distancia do objeto a frente;
