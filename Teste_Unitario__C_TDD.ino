10f7390d342d0bdb910b08570413015779c7f7b2
juniorcarvalho07 <junior_sousa95@hotmail.com>
2017-09-27 01:01:25 -0300

    Codigo de teste unitario em C(C+TDD)

---
+++ Teste_Unitario__C_TDD.ino
@@ -0,0 +1,22 @@
+#include <assert.h>
+  
+double somar(double a, double b){
+       return a + b;
+} 
+
+
+static void testsomar(){
+  double resultadoespe = 7.5;
+  double resultado =  somar(3.5,4.0);
+  assert(resultadoespe == somar(3.5,3.0) && "testsomar()");
+}
+void setup() {
+Serial.begin(9600);
+}
+
+
+void loop() {
+testsomar();
+Serial.println(somar(3.5,4.0));
+
+}
