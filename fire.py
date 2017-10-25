af3208168e9a51f8fe255d973b27a0498a725b9f
juniorcarvalho07 <junior_sousa95@hotmail.com>
2017-10-25 02:14:05 -0300

    uma part do servidor para receber os dados do arduino com e comunicar com o firebase

--- fire.py
+++ fire.py
@@ -6,6 +6,26 @@ firebase_url = 'https://minicursoifpi.firebaseio.com/'
 #Connect to Serial Port for communication
 objeto_porta = serial.Serial('COM3', 9600)
 objeto_porta.flushInput();
-#Setup a loop to send Temperature values at fixed intervals
-#in seconds
 fixed_interval = 10
+while 1:
+	try:
+		def split1000(s, sep=','):
+            return s if len(s) <= 2 else split1000(s[:-2], sep) + sep + s[-2:]
+		
+		ultrasonic1 = str(objeto_porta.readline())
+		
+		time_hhmmss = time.strftime('%H:%M:%S')
+		date_mmddyyyy = time.strftime('%d/%m/%Y')
+		
+		ata = {'Distancia sensor 1':tempF2}
+		result = requests.post(firebase_url + '#', data=json.dumps(data))#preciso  criar um arquivo json 
+
+		
+		
+		
+		
+		
+    time.sleep(fixed_interval)
+  except IOError:
+    print('Error! Something went wrong.')
+  time.sleep(fixed_interval)
