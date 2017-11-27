d3ba6f340bd25d147fe29755b9714f4c8d2b000b
DanielaSenna <daany_senna07@hotmail.com>
2017-11-27 20:26:00 -0300

    Teste 1 para verificar Conexão Bluetooth

---
+++ Visual/app/src/test/java/com/example/danielasenna/visual_iot/ConnectionThreadTest.java
@@ -0,0 +1,22 @@
+package com.example.danielasenna.visualiot;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Daniela Senna on 26/11/2017.
+ */
+public class ConnectionThreadTest {
+    ConnectionThread c = new ConnectionThread();
+
+    @Test
+    public void connection() throws Exception {
+        boolean a = c.server;
+
+        assertTrue(a);
+    }
+
+   
+}
